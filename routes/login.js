e0817de442f77e0ca9edf1eec2b1ecfbaf12c4d7
Rahul <rahulkvr7a.rnj@gmail.com>
2021-09-07 16:30:13 +0530

    second

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+const username = "rahul";
+const password = "123";
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.render('Login');
+  res.render('login');
 });
+
+router.post('/login', (req, res) => {
+  if (username == req.body.username && password == req.body.password) {
+    res.redirect('/index')
+  } else {
+    res.redirect('/login')
+  }
+})
 
 module.exports = router;
